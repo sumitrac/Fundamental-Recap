ab1c809891ce18020fb444851c73e3a9e4962af0
Sumitra <sumitrac2008@gmail.com>
2021-03-11 17:03:43 -0800

    rename file

---
+++ examples.py
@@ -0,0 +1,8 @@
+# int(False)
+
+# print("Hello world")
+# print(type("I'm programming!"))
+# age = 99
+# print("Tomorrow, I will be {age + 1} years old!")
+# age = 99
+# print("Tomorrow, I will be", age + 1, "years old!")
